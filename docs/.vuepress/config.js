365368ed0ba621c00c6ee841ba0cddf227aac35b
luotao <26993@163.com>
2020-08-22 21:04:58 +0800

    初始化安装

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    title: '个人文档',
+    description: '爱生活，爱自由！',
+    themeConfig: {
+        logo: '/assets/img/logo.png',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            {
+                text: '了解更多',
+                items: [
+                    { text: 'Api', items: [
+                            { text: 'CLI', link: '/guide/' },
+                            { text: 'Node', link: '/guide/' },
+                        ] },
+                    { text: '开发指南', items: [
+                            { text: '本地开发', link: '/guide/' },
+                            { text: '设计理念', link: '/guide/' },
+                            { text: 'FAQ', link: '/guide/' },
+                            { text: '术语', link: '/guide/' },
+                        ] },
+                    { text: '其他', items: [
+                            { text: '从 0.x 迁移', link: '/guide/' },
+                            { text: 'Changelog ', link: '/guide/', target:'_blank'},
+                        ] }
+                ]
+            },
+            { text: 'Github', link: 'https://github.com/eluotao', target:'_blank'},
+        ]
+    }
+}
